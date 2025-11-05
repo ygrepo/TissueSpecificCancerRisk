2f3c3abe9b073b91b7801acc14fa552ae6e46af8
Yves Greatti <ygrepo@gmail.com>
2025-11-05 17:04:08 -0500

    Debugging

--- jobs/corrupt_infer_3.sh
+++ jobs/corrupt_infer_3.sh
@@ -20,20 +20,23 @@ module load java/1.8.0_151
 export SITKA_BIN_PATH="/sc/arion/projects/DiseaseGeneCell/Huang_lab_project/sitkatree/sitka/build/install/nowellpack/bin"
 export PATH=$SITKA_BIN_PATH:$PATH
 
+mkdir -p logs
+
 # Run the main inference command
 # We set --engine.nChains to 4 to match our '-n 4' CPU request
 FILTERED_PATH=/sc/arion/projects/DiseaseGeneCell/Huang_lab_project/TissueSpecificCancerRisk/results/all/2025-11-05-10-32-39-xmCj8wy3.exec/filtered.csv
 echo "Running"
-corrupt-infer-with-noisy-params \
-    --model.globalParameterization true \
-    --model.binaryMatrix filtered.csv \
-    --model.fprBound 0.1 \
-    --model.fnrBound 0.5 \
-    --engine PT \
-    --engine.initialization FORWARD \
-    --engine.nScans 10000 \
-    --engine.nPassesPerScan 1 \
-    --engine.nChains 10
+corrupt-infer-with-noisy-params -h
+# \
+#     --model.globalParameterization true \
+#     --model.binaryMatrix filtered.csv \
+#     --model.fprBound 0.1 \
+#     --model.fnrBound 0.5 \
+#     --engine PT \
+#     --engine.initialization FORWARD \
+#     --engine.nScans 10000 \
+#     --engine.nPassesPerScan 1 \
+#     --engine.nChains 10
 
 cp results/latest/samples/phylo.csv ./
 echo "Job finished."
