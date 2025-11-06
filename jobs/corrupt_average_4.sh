e311739844d50a386c43ee641e335b691ce7783b
Yves Greatti <ygrepo@gmail.com>
2025-11-06 09:09:23 -0500

    Fixing scripts

--- jobs/corrupt_average_4.sh
+++ jobs/corrupt_average_4.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-# --- LSF Job Options ---
 #BSUB -P acc_DiseaseGeneCell
-#BSUB -J sitka_average      # Job name
-#BSUB -n 1                    # Request 1 CPU core
-#BSUB -W 2:00                 # Walltime of 2 hours
-#BSUB -M 32000                # Request 32 GB of memory
-#BSUB -o logs/average.%J.out    # Standard output log file
-#BSUB -e logs/average.%J.err    # Standard error log file
-# --- End LSF Options ---
+#BSUB -J sitka_average
+#BSUB -n 1
+#BSUB -W 2:00
+#BSUB -M 32000
+#BSUB -o logs/average.%J.out
+#BSUB -e logs/average.%J.err
 
 echo "Running corrupt-average on host $(hostname)..."
 
 PROJECT_DIR="/sc/arion/projects/DiseaseGeneCell/Huang_lab_project/TissueSpecificCancerRisk"
 echo "Changing to working directory: ${PROJECT_DIR}"
 cd "${PROJECT_DIR}"
+
 # Load the required Java module
 module purge
 module load java/1.8.0_151
-
 
 # Add the sitkatree binaries to your PATH
 export SITKA_BIN_PATH="/sc/arion/projects/DiseaseGeneCell/Huang_lab_project/sitkatree/sitka/build/install/nowellpack/bin"
