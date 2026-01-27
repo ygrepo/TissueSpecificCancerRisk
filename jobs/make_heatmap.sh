586befa769caf7dc18ced0f0041f2dd50d4cc5a4
Yves Greatti <ygrepo@gmail.com>
2026-01-27 14:06:18 -0500

    Regenerating plots

--- jobs/make_heatmap.sh
+++ jobs/make_heatmap.sh
@@ -40,31 +40,31 @@ echo "Starting heatmap generation..."
 #     --linkheight 2
 
 # SA501
-# Rscript src/make_heatmap.R \
-#     --treefile "trees/SA501/SA501.tree.2.newick" \
-#     --cnv_data "data/SA501.tbnc.cnv.csv" \
-#     --output "output/figures/SA501_heatmap_chr13_17q_del_2.pdf" \
-#     --chr13_17_deletion \
-#     --chr13_17_deletion_threshold 0.5 \
-#     --plot_tree \
-#     --width 8 \
-#     --height 6 \
-#     --title "SA501 TBC Clonal Evolution chr13/17q Deletions" \
-#     --tree_width 3 \
-#     --linkheight 1
-     
 Rscript src/make_heatmap.R \
-    --treefile "trees/B218/tree.newick" \
-    --cnv_data "data/B2HET18-hscn.csv" \
-    --output "output/figures/B218_heatmap_chr13_17q_del.pdf" \
+    --treefile "trees/SA501/SA501.tree.2.newick" \
+    --cnv_data "data/SA501.tbnc.cnv.csv" \
+    --output "output/figures/SA501_heatmap_chr13_17q_del.pdf" \
     --chr13_17_deletion \
-    --chr13_17_deletion_threshold 0.25 \
+    --chr13_17_deletion_threshold 0.5 \
     --plot_tree \
     --width 8 \
     --height 6 \
-    --title "B218 Clonal Evolution chr13/17q Deletions" \
+    --title "SA501 TBC Clonal Evolution chr13/17q Deletions" \
     --tree_width 3 \
     --linkheight 1
+     
+# Rscript src/make_heatmap.R \
+#     --treefile "trees/B218/tree.newick" \
+#     --cnv_data "data/B2HET18-hscn.csv" \
+#     --output "output/figures/B218_heatmap_chr13_17q_del.pdf" \
+#     --chr13_17_deletion \
+#     --chr13_17_deletion_threshold 0.25 \
+#     --plot_tree \
+#     --width 8 \
+#     --height 6 \
+#     --title "B218 Clonal Evolution chr13/17q Deletions" \
+#     --tree_width 3 \
+#     --linkheight 1
 
 # Rscript src/make_heatmap.R \
 #     --treefile "trees/B216/tree.newick" \
